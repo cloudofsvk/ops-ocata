50e6d5c6145fe2ec49e187647827faad6cf76b29
svk <svk@example.com>
2017-05-23 17:44:29 +0530

    Updated file

--- 2-network-config.sh
+++ 2-network-config.sh
@@ -5,12 +5,11 @@
 ################################################### 
 Node_Inst_Start () {
 	echo -e  "######\e[92m Starting ---- $1 ---- Configuration \e[0m######"
-	echo -e "#####\e[31m Update and Upgrade the Server Before starting the Installation \e[0m#####"
 	sleep 2
-	read -rsp $' ... If Updated \e[92mPress any key\e[0m to Continue OR \e[31mCTRL+C\e[0m to Exit Installation ... \n' -n1 key
+	read -rsp $' ... \e[92mPress any key\e[0m to Continue OR \e[31mCTRL+C\e[0m to Exit Configuration ... \n' -n1 key
 } 
 Key_To_Start () {
-	read -rsp $' ... \e[92mPress any key\e[0m to Continue OR \e[31mCTRL+C\e[0m to Exit Installation ... \n' -n1 key
+	read -rsp $' ... \e[92mPress any key\e[0m to Continue OR \e[31mCTRL+C\e[0m to Exit Configuration ... \n' -n1 key
 }
 Key_To_Exit () {
 	read -rsp $' ... \e[92mPress any key\e[0m to Exit ... \n' -n1 key
